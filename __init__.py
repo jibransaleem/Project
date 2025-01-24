a90ae6b7910e5df4295950d156169541ed856752
jibransaleem <saleemjibran8123#gmail.com>
2025-01-24 11:39:10 +0500

    added pass

--- __init__.py
+++ __init__.py
@@ -2,9 +2,10 @@ import re
 import datetime as dt
 import phonenumbers
 import requests
-from dotenv import load_dotenv   
+#from dotenv import load_dotenv   
+import bcrypt
 import os
-import json
+import random
 class Auth:
     def __init__(self):
         pass
@@ -58,4 +59,45 @@ class Auth:
                 
         except requests.exceptions.RequestException as e:
             print(e)
+    def Password(self):
+        choise : str = input('Enter Y to create your own password else hit any button ! ').strip().lower()
+        if choise!='y':
+            a_z = [ord(i) for i in range(97,123)]
+            A_Z = [ord(i) for i in range(65,91)]
+            spec =list("!#$%&'()*+,-./:;<=>?@[\]^_`{|}~")
+            key = []
+            for i in range(1,3):
+                index = random.randint(0,26)
+                key.append(a_z[index])
+            random.shuffle(key)
+            for i in range(1,2):
+                index = random.randint(0,26)
+                key.append(A_Z[index])
+            random.shuffle(key)
+            for i in range(1,3):
+                index = random.randint(0,len(spec))
+                key.append(spec[index])
+            random.shuffle(key)
+            for i in range(random.randint(1,3)):
+                key.apppend(str(random.randint(0,9)))
+            random.shuffle(key)
+            if len(key)<7:
+                for i in range(7-len(key)):
+                    index = random.randint(0,26)
+                    key.append(A_Z[index])
+            random.shuffle(key)
+            key = "".join(key)
+        else:
+            key = input('Enter password (length should be greater than 6 ) :').strip()
+            while(len(key<6)):
+                key = input('Please enter password according to rule (length should be greater than 6 ) :').strip()
+           
+            self.__hashed = bcrypt.hashpw(key.encode('utf-8') , bcrypt.gensalt())
+            #if bcrypt.checkpw(vld.encode('utf-8'),self.ContactNumber__hashed):
+                        
             
+        
+        
+
+
+             
